566ce9b439ac54b0392d464aef63b9093a804f50
Daniyal <134530572+daniyalshagiroff@users.noreply.github.com>
2025-04-17 23:07:24 +0400

    Update validation.py

--- src/validation.py
+++ src/validation.py
@@ -3,32 +3,32 @@ import joblib
 from data_load import load_data
 from preprocessing import clean_text
 
-# Проверка модели на другом датасете
+# Evaluating the model on a different dataset
 def evaluate_model_on_new_data(model_path, vectorizer_path, test_data_path):
-    # Загрузка модели и векторизатора
+    # Load the model and vectorizer
     model = joblib.load(model_path)
     vectorizer = joblib.load(vectorizer_path)
     print("Model and vectorizer loaded.")
 
-    # Загрузка нового датасета
+    # Load the new dataset
     test_df = load_data(test_data_path)
     print("Test data loaded.")
 
-    # Очистка текста
+    # Clean the text
     test_df["cleaned"] = test_df[3].apply(clean_text)
     test_df = test_df.dropna(subset=["cleaned"])
     X_test = test_df["cleaned"]
     y_test = test_df[2]
     print("Text cleaning completed.")
 
-    # Преобразование текста в векторное представление
+    # Transform text into vector representation
     X_test_vec = vectorizer.transform(X_test)
     print("Vectorization of test data completed.")
 
-    # Предсказания
+    # Make predictions
     y_pred = model.predict(X_test_vec)
 
-    # Оценка точности
+    # Evaluate accuracy
     accuracy = accuracy_score(y_test, y_pred)
     print("Accuracy on new dataset:", accuracy)
 
